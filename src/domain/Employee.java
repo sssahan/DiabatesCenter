302a8950252573f7af463005fa69bcbb46df06ff
sachithra sahan <sachithrasahansamarasinghe@gmail.com>
2016-03-22 15:35:11 +0530

    some implementations in patient and employee

--- src/domain/Employee.java
+++ src/domain/Employee.java
@@ -10,5 +10,96 @@ package domain;
  * @author sachithra sahan
  */
 public class Employee {
+    private int employeeID;    
+    private String position;
+    private String name;
+    private String NIC;
+    private String username;
+    private String password;
+
+    /**
+     * @return the employeeID
+     */
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    /**
+     * @param employeeID the employeeID to set
+     */
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    /**
+     * @return the position
+     */
+    public String getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the NIC
+     */
+    public String getNIC() {
+        return NIC;
+    }
+
+    /**
+     * @param NIC the NIC to set
+     */
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     
 }
--- src/domain/Patient.java
+++ src/domain/Patient.java
@@ -10,5 +10,128 @@ package domain;
  * @author sachithra sahan
  */
 public class Patient {
+    private int PatientID;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int age;
+    private String bloodGroup;
+    private String address;
+    private String phoneNum;
+
+    /**
+     * @return the PatientID
+     */
+    public int getPatientID() {
+        return PatientID;
+    }
+
+    /**
+     * @param PatientID the PatientID to set
+     */
+    public void setPatientID(int PatientID) {
+        this.PatientID = PatientID;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the bloodGroup
+     */
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    /**
+     * @param bloodGroup the bloodGroup to set
+     */
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the phoneNum
+     */
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    /**
+     * @param phoneNum the phoneNum to set
+     */
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+   
+    
     
 }
