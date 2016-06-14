c9141a730bdad81356383e877b3e1941ebb990aa
sachithra sahan <sachithrasahansamarasinghe@gmail.com>
2016-06-14 11:44:09 +0530

    add look and feel library

--- src/diabetescenter/DiabetesCenter.java
+++ src/diabetescenter/DiabetesCenter.java
@@ -5,6 +5,10 @@
  */
 package diabetescenter;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import view.LoginWindow;
 
 /**
@@ -17,7 +21,12 @@ public class DiabetesCenter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            // TODO code application logic here
+            UIManager.setLookAndFeel("com.jtattoo.plaf.mcwin.McWinLookAndFeel");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(DiabetesCenter.class.getName()).log(Level.SEVERE, null, ex);
+        }
         LoginWindow loginWindow=new LoginWindow();
         loginWindow.setLocationRelativeTo(null);
         loginWindow.setVisible(true);
