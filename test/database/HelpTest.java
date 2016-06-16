0bd6b59bb872c277bd1b7e896a54bed515134bf3
sachithra sahan <sachithrasahansamarasinghe@gmail.com>
2016-06-16 17:27:31 +0530

    add test cases

--- test/database/HelpTest.java
+++ test/database/HelpTest.java
@@ -50,8 +50,7 @@ public class HelpTest {
         Date expResult = new Date(year-1900,month-1,day);
         Date result = Help.getDate(year, month, day);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        
     }
 
     /**
@@ -60,12 +59,11 @@ public class HelpTest {
     @Test
     public void testGetYear() {
         System.out.println("getYear");
-        Date date = new Date(2016-1900,05-1,25);;
+        Date date = new Date(2016-1900,05-1,25);
         int expResult =2016;
         int result = Help.getYear(date);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        
     }
 
     /**
@@ -74,12 +72,11 @@ public class HelpTest {
     @Test
     public void testGetMonth() {
         System.out.println("getMonth");
-        Date date = null;
-        int expResult = 0;
+        Date date = new Date(2016-1900,10-1,15);
+        int expResult = 10;
         int result = Help.getMonth(date);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
@@ -88,12 +85,11 @@ public class HelpTest {
     @Test
     public void testGetDay() {
         System.out.println("getDay");
-        Date date = null;
-        int expResult = 0;
+        Date date = new Date(2016-1900,05-1,12);
+        int expResult = 12;
         int result = Help.getDay(date);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
@@ -102,12 +98,11 @@ public class HelpTest {
     @Test
     public void testIsValidDate() {
         System.out.println("isValidDate");
-        String inDate = "";
-        boolean expResult = false;
+        String inDate = "2015-02-06";
+        boolean expResult = true;
         boolean result = Help.isValidDate(inDate);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
     
 }
